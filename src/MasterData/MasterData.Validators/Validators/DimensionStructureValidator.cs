8e995cb7220e79f139932fa2476efb6e0ab6ebed
András Csányi <sayusi.ando@gmail.com>
2020-01-17 23:39:44 +0100

    Revert "Feature/masterdata/6 modify dimension structure"

--- src/MasterData/MasterData.Validators/Validators/DimensionStructureValidator.cs
+++ src/MasterData/MasterData.Validators/Validators/DimensionStructureValidator.cs
@@ -28,26 +28,6 @@ namespace DigitalLibrary.MasterData.Validators.Validators
                     RuleFor(p => p.ParentDimensionStructureId).NotNull().NotEqual(0);
                 });
             });
-            
-            RuleSet(ValidatorRulesets.UpdateDimensionStructure, () =>
-            {
-                When(w => w.Name == null || w.Desc == null, () =>
-                {
-                    RuleFor(e => e.Name).NotNull();
-                    RuleFor(e => e.Desc).NotNull();
-                });
-
-                When(w => w.Name != null && w.Desc != null, () =>
-                {
-                    RuleFor(p => p.Id).NotEqual(0);
-                    RuleFor(p => p.Name).NotEmpty().NotEqual(" ");
-                    RuleFor(p => p.Name.Length).GreaterThanOrEqualTo(3);
-                    RuleFor(p => p.Desc).NotEmpty().NotEqual(" ");
-                    RuleFor(p => p.Desc.Length).GreaterThanOrEqualTo(3);
-                    RuleFor(p => p.IsActive).GreaterThanOrEqualTo(0).LessThanOrEqualTo(1);
-                    RuleFor(p => p.ParentDimensionStructureId).NotNull().NotEqual(0);
-                });
-            });
         }
     }
 }
