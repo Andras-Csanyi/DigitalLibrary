45b54bcac04a2ee272d29e41559976682cecf31d
SayusiAndo <sayusi.ando@gmail.com>
2020-01-24 21:27:26 +0100

    #29 add validation

---
+++ src/MasterData/MasterData.Controllers.Integration.Tests/Tests/Dimension/Add_Validation_Should.cs
@@ -0,0 +1,72 @@
+namespace DigitalLibrary.MasterData.Controllers.Integration.Tests.Dimension
+{
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    using DomainModel;
+
+    using FluentAssertions;
+
+    using Utils.IntegrationTestFactories.Factories;
+
+    using Validators.TestData;
+
+    using WebApi.Client;
+
+    using WebApp;
+
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class Add_Validation_Should : TestBase<Dimension>
+    {
+        public Add_Validation_Should(DiLibMasterDataWebApplicationFactory<Startup, Dimension> host,
+                                     ITestOutputHelper testOutputHelper) : base(host, testOutputHelper)
+        {
+        }
+
+        [Fact]
+        public async Task ThrowException_WhenInputIsNull()
+        {
+            // Arrange
+
+            // Act
+            Func<Task> action = async () =>
+            {
+                await masterDataHttpClient.AddDimensionAsync(null).ConfigureAwait(false);
+            };
+
+            // Assert
+            action.Should().ThrowExactly<MasterDataHttpClientException>();
+        }
+
+        [Theory]
+        [MemberData(nameof(MasterData_Dimension_TestData.AddDimensionAsync_Validation),
+            MemberType = typeof(MasterData_Dimension_TestData))]
+        public async Task ThrowException_WhenInputIsInvalid(
+            long id,
+            string name,
+            string desc,
+            int isActive)
+        {
+            // Arrange
+            Dimension dimension = new Dimension
+            {
+                Id = id,
+                Name = name,
+                Description = desc,
+                IsActive = isActive
+            };
+
+            // Act
+            Func<Task> action = async () =>
+            {
+                await masterDataHttpClient.AddDimensionAsync(dimension).ConfigureAwait(false);
+            };
+
+            // Assert
+            action.Should().ThrowExactly<MasterDataHttpClientException>();
+        }
+    }
+}
