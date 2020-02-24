24b2325a4dac82a5b3804def9e193949170178f7
SayusiAndo <sayusi.ando@gmail.com>
2020-02-24 22:02:48 +0100

    #25 SourceFormatBuilderService further tests

--- src/Host/WebUI.Test/SourceFormatBuilderService/RemoveItemFromTreeAsync_Should.cs
+++ src/Host/WebUI.Test/SourceFormatBuilderService/RemoveItemFromTreeAsync_Should.cs
@@ -239,16 +239,128 @@ namespace WebUI.Test.SourceFormatBuilderService
         [Fact]
         public async Task RemoveItem_WhenSingleItemOnTheSecondLevel()
         {
-        }
+            // Arrange
+            DimensionStructure secondLevelFirst = new DimensionStructure
+            {
+                Id = 201,
+                Name = "two oh one",
+                Desc = "two oh one",
+            };
+            DimensionStructure secondLevelSecond = new DimensionStructure
+            {
+                Id = 202,
+                Name = "two oh two",
+                Desc = "two oh two",
+            };
+            DimensionStructure firstLevelThird = new DimensionStructure
+            {
+                Id = 103,
+                Name = "one of three",
+                Desc = "one oh three",
+                ChildDimensionStructures = new List<DimensionStructure>
+                {
+                    secondLevelFirst,
+                    secondLevelSecond
+                },
+            };
+            DimensionStructure firstLevelFirst_secondLevelFirst = new DimensionStructure
+            {
+                Id = 1201,
+                Name = "two oh one",
+                Desc = "two oh one",
+            };
+            DimensionStructure firstLevelFirst_secondLevelSecond = new DimensionStructure
+            {
+                Id = 1202,
+                Name = "two oh two",
+                Desc = "two oh two",
+            };
+            DimensionStructure firstLevelFirst = new DimensionStructure
+            {
+                Id = 101,
+                Name = "one oh one",
+                Desc = "one oh one",
+                ChildDimensionStructures = new List<DimensionStructure>
+                {
+                    firstLevelFirst_secondLevelFirst,
+                    firstLevelFirst_secondLevelSecond,
+                },
+            };
+            DimensionStructure firstLevelSecond = new DimensionStructure
+            {
+                Id = 102,
+                Name = "two oh two",
+                Desc = "two oh two",
+            };
+            List<DimensionStructure> childDimensionStructures = new List<DimensionStructure>
+            {
+                firstLevelFirst,
+                firstLevelSecond,
+                firstLevelThird,
+            };
+            DimensionStructure rootDimensionStructure = new DimensionStructure
+            {
+                Id = 100,
+                Name = "root",
+                Desc = "root",
+                ChildDimensionStructures = childDimensionStructures,
+            };
+            SourceFormat testData = new SourceFormat
+            {
+                Id = 100,
+                Name = "test",
+                Desc = "test desc",
+                RootDimensionStructureId = 100,
+                RootDimensionStructure = rootDimensionStructure,
+            };
 
-        [Fact]
-        public async Task RemoveItemWithItsChildren_WhenMultlpleElemsAreOnTheSecondLevel()
-        {
-        }
+            _masterDataWebApiClientMock
+               .Setup(m => m.GetSourceFormatWithFullDimensionStructureTreeAsync(It.IsAny<SourceFormat>()))
+               .ReturnsAsync(testData);
 
-        [Fact]
-        public async Task ReturnsWithTheTreeUnchanged_WhenThereIsNoItemToBeRemoved()
-        {
+            ISourceFormatBuilderService builderService = new SourceFormatBuilderService(
+                _masterDataWebApiClientMock.Object);
+            await builderService.OnUpdate(100).ConfigureAwait(false);
+
+            // Act
+            await builderService.DeleteDocumentStructureFromTreeAsync(1202).ConfigureAwait(false);
+
+            // Assert
+            builderService.SourceFormat.Should().NotBeNull();
+            builderService.SourceFormat.Id.Should().Be(testData.Id);
+            builderService.SourceFormat.Name.Should().Be(testData.Name);
+            builderService.SourceFormat.Desc.Should().Be(testData.Desc);
+
+            builderService.SourceFormat.RootDimensionStructure.Id.Should().Be(rootDimensionStructure.Id);
+
+            // checking first level
+            builderService.SourceFormat.RootDimensionStructure.ChildDimensionStructures.Count.Should().Be(3);
+            builderService.SourceFormat.RootDimensionStructure.ChildDimensionStructures
+               .Where(p => p.Id == firstLevelFirst.Id).ToList().Count.Should().Be(1);
+            builderService.SourceFormat.RootDimensionStructure.ChildDimensionStructures
+               .Where(p => p.Id == firstLevelSecond.Id).ToList().Count.Should().Be(1);
+            builderService.SourceFormat.RootDimensionStructure.ChildDimensionStructures
+               .Where(p => p.Id == firstLevelThird.Id).ToList().Count.Should().Be(1);
+
+            builderService.SourceFormat.RootDimensionStructure.ChildDimensionStructures
+               .FirstOrDefault(p => p.Id == firstLevelFirst.Id)
+               .ChildDimensionStructures.Count.Should().Be(1);
+            builderService.SourceFormat.RootDimensionStructure.ChildDimensionStructures
+               .FirstOrDefault(p => p.Id == firstLevelFirst.Id)
+               .ChildDimensionStructures
+               .Where(q => q.Id == firstLevelFirst_secondLevelFirst.Id).ToList().Count.Should().Be(1);
+
+            builderService.SourceFormat.RootDimensionStructure.ChildDimensionStructures
+               .FirstOrDefault(p => p.Id == firstLevelThird.Id)
+               .ChildDimensionStructures.Count.Should().Be(2);
+            builderService.SourceFormat.RootDimensionStructure.ChildDimensionStructures
+               .FirstOrDefault(p => p.Id == firstLevelThird.Id)
+               .ChildDimensionStructures
+               .Where(q => q.Id == secondLevelFirst.Id).ToList().Count.Should().Be(1);
+            builderService.SourceFormat.RootDimensionStructure.ChildDimensionStructures
+               .FirstOrDefault(p => p.Id == firstLevelThird.Id)
+               .ChildDimensionStructures
+               .Where(q => q.Id == secondLevelSecond.Id).ToList().Count.Should().Be(1);
         }
     }
 }
