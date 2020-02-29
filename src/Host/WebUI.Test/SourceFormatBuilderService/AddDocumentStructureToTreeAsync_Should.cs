711b5aea10114d116a682f0c0726559dea030aa5
SayusiAndo <sayusi.ando@gmail.com>
2020-02-29 17:03:34 +0100

    #25 AddDocumentStructureToTreeAsync_Should

--- src/Host/WebUI.Test/SourceFormatBuilderService/AddDocumentStructureToTreeAsync_Should.cs
+++ src/Host/WebUI.Test/SourceFormatBuilderService/AddDocumentStructureToTreeAsync_Should.cs
@@ -34,7 +34,8 @@ namespace WebUI.Test.SourceFormatBuilderService
                 RootDimensionStructure = rootDimensionStructure,
                 RootDimensionStructureId = rootDimensionStructure.Id,
             };
-            _masterDataWebApiClientMock.Setup(m => m.GetSourceFormatWithFullDimensionStructureTreeAsync(
+            _masterDataWebApiClientMock
+               .Setup(m => m.GetSourceFormatWithFullDimensionStructureTreeAsync(
                     It.IsAny<SourceFormat>()))
                .ReturnsAsync(testData);
 
@@ -74,24 +75,105 @@ namespace WebUI.Test.SourceFormatBuilderService
         [Fact]
         public async Task AddItemTo_FirstLevel_EmptyLengthList()
         {
+            // Arrange
+            DimensionStructure rootDimensionStructure = new DimensionStructure
+            {
+                Id = 101,
+                Name = "101",
+                Desc = "101",
+                ChildDimensionStructures = new List<DimensionStructure>(),
+            };
+            SourceFormat testData = new SourceFormat
+            {
+                Id = 100,
+                Name = "name",
+                Desc = "desc",
+                RootDimensionStructure = rootDimensionStructure,
+                RootDimensionStructureId = rootDimensionStructure.Id,
+            };
+            _masterDataWebApiClientMock
+               .Setup(m => m.GetSourceFormatWithFullDimensionStructureTreeAsync(
+                    It.IsAny<SourceFormat>()))
+               .ReturnsAsync(testData);
+
+            DimensionStructure addedToRoot = new DimensionStructure
+            {
+                Id = 200,
+                Name = "Add by mock",
+                Desc = "Added by mock desc",
+                ChildDimensionStructures = new List<DimensionStructure>(),
+            };
+            _masterDataWebApiClientMock
+               .Setup(m => m.GetDimensionStructureByIdAsync(
+                    It.IsAny<DimensionStructure>()
+                ))
+               .ReturnsAsync(addedToRoot);
+
+            SourceFormatBuilderService sourceFormatBuilderService = new SourceFormatBuilderService(
+                _masterDataWebApiClientMock.Object);
+            await sourceFormatBuilderService.OnUpdate(100).ConfigureAwait(false);
+
+            // Adding to root level
+            await sourceFormatBuilderService.AddDocumentStructureToTreeAsync(200, 101).ConfigureAwait(false);
+
+            // Act
+            await sourceFormatBuilderService.AddDocumentStructureToTreeAsync(201, 200).ConfigureAwait(false);
+
+            // Assert
+            sourceFormatBuilderService.SourceFormat.Should().NotBeNull();
+            sourceFormatBuilderService.SourceFormat.Id.Should().Be(testData.Id);
+            sourceFormatBuilderService.SourceFormat.Name.Should().Be(testData.Name);
+            sourceFormatBuilderService.SourceFormat.Desc.Should().Be(testData.Desc);
+            sourceFormatBuilderService.SourceFormat.RootDimensionStructure.Id.Should().Be(rootDimensionStructure.Id);
+            sourceFormatBuilderService.SourceFormat.RootDimensionStructure.Name
+               .Should().Be(rootDimensionStructure.Name);
+            sourceFormatBuilderService.SourceFormat.RootDimensionStructure.Desc
+               .Should().Be(rootDimensionStructure.Desc);
+
+            sourceFormatBuilderService.SourceFormat.RootDimensionStructure
+               .ChildDimensionStructures
+               .Should().NotBeEmpty();
+            sourceFormatBuilderService.SourceFormat.RootDimensionStructure
+               .ChildDimensionStructures
+               .Where(m => m.Id == addedToRoot.Id).ToList().Count.Should().Be(1);
+            sourceFormatBuilderService.SourceFormat.RootDimensionStructure
+               .ChildDimensionStructures
+               .FirstOrDefault(m => m.Id == addedToRoot.Id)
+               .ChildDimensionStructures.Count.Should().Be(1);
         }
 
-        [Fact]
+        /// <summary>
+        /// Until this: https://stackoverflow.com/questions/60441646/how-to-setup-moq-for-the-same-method-where-return-value-depends-on-input
+        /// is not solved this part of the testing is suspended
+        /// </summary>
+        /// <returns></returns>
         public async Task AddItemTo_FirstLevel_ListHasMultipleItems()
         {
         }
 
-        [Fact]
+        /// <summary>
+        /// Until this: https://stackoverflow.com/questions/60441646/how-to-setup-moq-for-the-same-method-where-return-value-depends-on-input
+        /// is not solved this part of the testing is suspended
+        /// </summary>
+        /// <returns></returns>
         public async Task AddItemTo_SecondLevel_EmptyList()
         {
         }
 
-        [Fact]
+        /// <summary>
+        /// Until this: https://stackoverflow.com/questions/60441646/how-to-setup-moq-for-the-same-method-where-return-value-depends-on-input
+        /// is not solved this part of the testing is suspended
+        /// </summary>
+        /// <returns></returns>
         public async Task AddItemTo_SecondLevel_LastHasMultipleItems()
         {
         }
 
-        [Fact]
+        /// <summary>
+        /// Until this: https://stackoverflow.com/questions/60441646/how-to-setup-moq-for-the-same-method-where-return-value-depends-on-input
+        /// is not solved this part of the testing is suspended
+        /// </summary>
+        /// <returns></returns>
         public async Task AddItemTo_WhenAnItemIsAdded()
         {
         }
