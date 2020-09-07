8667d23853877051944127b324b9140cfb465d58
SayusiAndo <sayusi.ando@gmail.com>
2020-09-07 21:08:40 +0200

    TestBase

--- src/MasterData/MasterData.BusinessLogic.Implementations.Tests/SourceFormat/SourceFormatFeature.Background.cs
+++ src/MasterData/MasterData.BusinessLogic.Implementations.Tests/SourceFormat/SourceFormatFeature.Background.cs
@@ -25,60 +25,11 @@ namespace DigitalLibrary.MasterData.BusinessLogic.Implementations.Tests.SourceFo
     [SuppressMessage("ReSharper", "InconsistentNaming", Justification = "Reviewed.")]
     [SuppressMessage("ReSharper", "CA1707", Justification = "Reviewed.")]
     [SuppressMessage("ReSharper", "SA1600", Justification = "Reviewed.")]
-    public partial class SourceFormatFeature
+    public partial class SourceFormatFeature : TestBase
     {
-        public static readonly ILoggerFactory LoggerFactory = Microsoft.Extensions.Logging.LoggerFactory
-           .Create(builder => { builder.AddDebug(); });
-
-        protected IMasterDataBusinessLogic _masterDataBusinessLogic;
-
-        private const string TestInfo = nameof(SourceFormatFeature);
-
-        [Background]
-        public void Background()
+        public SourceFormatFeature(string testInfo)
+            : base(nameof(SourceFormatFeature))
         {
-            string msg = $"{TestInfo} cannot be null, empty or whitespace!";
-            Check.NotNullOrEmptyOrWhitespace(TestInfo, msg);
-
-            DbContextOptions<MasterDataContext> _dbContextOptions =
-                new DbContextOptionsBuilder<MasterDataContext>()
-                   .UseSqlite($"Data Source = {TestInfo}.sqlite")
-
-                    // .UseNpgsql("Server=127.0.0.1;Port=5432;Database=dilib;User Id=andrascsanyi;")
-                   .UseLoggerFactory(LoggerFactory)
-                   .EnableDetailedErrors()
-                   .EnableSensitiveDataLogging()
-                   .Options;
-
-            DimensionValidator dimensionValidator = new DimensionValidator();
-            MasterDataDimensionValueValidator masterDataDimensionValueValidator =
-                new MasterDataDimensionValueValidator();
-            SourceFormatValidator sourceFormatValidator = new SourceFormatValidator();
-            DimensionStructureValidator dimensionStructureValidator = new DimensionStructureValidator();
-            DimensionStructureDimensionStructureValidator dimensionStructureDimensionStructureValidator =
-                new DimensionStructureDimensionStructureValidator();
-            DimensionStructureQueryObjectValidator dimensionStructureQueryObjectValidator =
-                new DimensionStructureQueryObjectValidator();
-
-            MasterDataValidators masterDataValidators = new MasterDataValidators(
-                dimensionValidator,
-                masterDataDimensionValueValidator,
-                sourceFormatValidator,
-                dimensionStructureValidator,
-                dimensionStructureDimensionStructureValidator,
-                dimensionStructureQueryObjectValidator);
-
-            "Given there is the MasterDataBusinessLogic"
-               .x(() => _masterDataBusinessLogic = new MasterDataBusinessLogic(
-                    _dbContextOptions,
-                    masterDataValidators));
-
-            using (MasterDataContext ctx = new MasterDataContext(_dbContextOptions))
-            {
-                ctx.Database.EnsureDeleted();
-                ctx.Database.EnsureCreated();
-                MasterDataDataSample.Populate(ctx);
-            }
         }
     }
 }
