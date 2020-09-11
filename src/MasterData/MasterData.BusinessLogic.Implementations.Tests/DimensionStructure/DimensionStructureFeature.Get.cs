17fea5b3c82bc200ac837a5adf63ead6874ed16f
SayusiAndo <sayusi.ando@gmail.com>
2020-09-11 18:12:07 +0200

    DimensionStructureFeature.Get xbehave

--- src/MasterData/MasterData.BusinessLogic.Implementations.Tests/DimensionStructure/DimensionStructureFeature.Get.cs
+++ src/MasterData/MasterData.BusinessLogic.Implementations.Tests/DimensionStructure/DimensionStructureFeature.Get.cs
@@ -24,39 +24,48 @@ namespace DigitalLibrary.MasterData.BusinessLogic.Implementations.Tests.Dimensio
     /// </summary>
     public partial class DimensionStructureFeature
     {
-        [Scenario(Skip = "Needs to be reviewed.")]
+        [Scenario]
         public async Task Get_AnItem()
         {
-            // Arrange
-            DimensionStructure dimensionStructure = new DimensionStructure
-            {
-                Name = "name",
-                Desc = "desc",
-                IsActive = 1,
-            };
-            DimensionStructure dimensionStructureResult = await _masterDataBusinessLogic.AddDimensionStructureAsync(
-                    dimensionStructure)
-               .ConfigureAwait(false);
+            DimensionStructure dimensionStructure = null;
+            "Given there is a dimension structure"
+               .x(() => dimensionStructure = new DimensionStructure
+                {
+                    Name = "name",
+                    Desc = "desc",
+                    IsActive = 1,
+                });
 
-            DimensionStructure dimensionStructure2 = new DimensionStructure
-            {
-                Name = "name2",
-                Desc = "desc2",
-                IsActive = 0,
-            };
-            DimensionStructure dimensionStructure2Result = await _masterDataBusinessLogic.AddDimensionStructureAsync(
-                    dimensionStructure2)
-               .ConfigureAwait(false);
+            "And it is stored in the database"
+               .x(async () => await _masterDataBusinessLogic.AddDimensionStructureAsync(
+                        dimensionStructure)
+                   .ConfigureAwait(false));
 
-            // Act
-            List<DimensionStructure> result = await _masterDataBusinessLogic.GetDimensionStructuresAsync()
-               .ConfigureAwait(false);
+            DimensionStructure dimensionStructure2 = null;
+            "And there is a dimension structure"
+               .x(() => dimensionStructure2 = new DimensionStructure
+                {
+                    Name = "name2",
+                    Desc = "desc2",
+                    IsActive = 0,
+                });
 
-            // Assert
-            result.Should().NotBeNull();
-            result.Count.Should().BeGreaterThan(0);
-            result.Where(p => p.Name == dimensionStructure.Name).ToList().Count.Should().Be(1);
-            result.Where(p => p.Name == dimensionStructure2.Name).ToList().Count.Should().Be(1);
+            "And it is stored in the database"
+               .x(async () => await _masterDataBusinessLogic.AddDimensionStructureAsync(
+                        dimensionStructure2)
+                   .ConfigureAwait(false));
+
+            List<DimensionStructure> result = null;
+            "When dimension structures list is queried"
+               .x(async () => result = await _masterDataBusinessLogic.GetDimensionStructuresAsync()
+                   .ConfigureAwait(false));
+
+            "Then result list is not null".x(() => result.Should().NotBeNull());
+            "And result list length is more than 0".x(() => result.Count.Should().BeGreaterThan(0));
+            "And first dimension structure is in the list"
+               .x(() => result.Where(p => p.Name == dimensionStructure.Name).ToList().Count.Should().Be(1));
+            "And second dimension structure is in the list"
+               .x(() => result.Where(p => p.Name == dimensionStructure2.Name).ToList().Count.Should().Be(1));
         }
     }
 }
