4493b05727833fc3cef136c368efce888bd5b4dd
SayusiAndo <sayusi.ando@gmail.com>
2020-09-14 20:02:42 +0200

    tmp

--- src/MasterData/MasterData.BusinessLogic.Interfaces/Interfaces/IMasterDataBusinessLogic.DimensionStructure.cs
+++ src/MasterData/MasterData.BusinessLogic.Interfaces/Interfaces/IMasterDataBusinessLogic.DimensionStructure.cs
@@ -23,7 +23,14 @@ namespace DigitalLibrary.MasterData.BusinessLogic.Interfaces
 
         Task<DimensionStructure> AddDimensionStructureAsync(DimensionStructure dimensionStructure);
 
-        Task<DimensionStructure> AddDimensionStructureToSourceformatAsync(
+        /// <summary>
+        /// It adds <see cref="DimensionStructure"/> to a <see cref="SourceFormat"/> as its
+        /// RootDimensionStructure.
+        /// </summary>
+        /// <param name="dimensionStructureId">The DimensionStructure will be added.</param>
+        /// <param name="sourceFormatId">The SourceFormat it wil be added to.</param>
+        /// <returns>The dimension structure.</returns>
+        Task<DimensionStructure> AddDimensionStructureToSourceFormatAsRootDimensionStructureAsync(
             long dimensionStructureId,
             long sourceFormatId);
 
