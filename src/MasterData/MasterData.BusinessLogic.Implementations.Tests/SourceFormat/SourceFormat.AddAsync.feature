3746a4921c720b9be4ee080a504a4abfa0a759ff
SayusiAndo <sayusi.ando@gmail.com>
2020-09-16 22:51:27 +0200

    tmp

--- src/MasterData/MasterData.BusinessLogic.Implementations.Tests/SourceFormat/SourceFormat.AddAsync.feature
+++ src/MasterData/MasterData.BusinessLogic.Implementations.Tests/SourceFormat/SourceFormat.AddAsync.feature
@@ -1,5 +1,10 @@
 Feature: AddAsync
   
+  As data structure owner and manager
+  I need functionality
+  Which provides me the ability to
+  Build and Manage SourceFormats
+  
   Scenario: Adds a SourceFormat when the newly added SourceFormat doesn't have RootDimensionStructure
     Given there is a SourceFormat without RootDimensionStructure
     When saving SourceFormat
@@ -9,3 +14,22 @@ Feature: AddAsync
     And the returned SourceFormat's name equals to the original's name
     And the returned SourceFormat's description equals to the original's description
     And the returned SourceFormat's is active value equals to the original's is active value
+    
+    Scenario: Adds a SourceFormat when the newly added SourceFormat has new RootDimensionStructure without dimension 
+    tree
+      Given there is a SourceFormat without RootDimensionStructure
+      And there is a DimensionStructure
+      And DimensionStructure is RootDimensionStructure of SourceFormat
+      When SourceFormat is saved
+      And it returns with the newly added SourceFormat
+      Then the returned SourceFormat is not null
+      And the returned SourceFormat's id should not be zero
+      And the returned SourceFormat's name equals to the original's name
+      And the returned SourceFormat's description equals to the original's description
+      And the returned SourceFormat's is active value equals to the original's is active value
+      And the returned SourceFormat's RootDimensionStructure is not null
+      And the returned SourceFormat's RootDimensionStructure Id is not zero
+      And the returned SourceFormat's RootDimensionStructure's name equals to original's name
+      And the returned SourceFormat's RootDimensionStructure's desc equals to original's desc
+      And the returned SourceFormat's RootDimensionStructure's is active equals to original's is active
+      
---
+++ src/MasterData/MasterData.BusinessLogic.Implementations.Tests/SourceFormat/SourceFormatAddAsyncStepDefs.cs
@@ -0,0 +1,96 @@
+namespace DigitalLibrary.MasterData.BusinessLogic.Implementations.Tests.SourceFormat
+{
+    using System.Threading.Tasks;
+
+    using DigitalLibrary.MasterData.DomainModel;
+
+    using FluentAssertions;
+
+    using TechTalk.SpecFlow;
+
+    using Xunit.Abstractions;
+
+    [Binding]
+    public class SourceFormatAddAsyncStepDefs : MasterDataBusinessLogicFeature
+    {
+        private SourceFormat _sourceFormat;
+
+        private SourceFormat _sourceFormatResult;
+
+        private DimensionStructure _dimensionStructure;
+
+        protected SourceFormatAddAsyncStepDefs(
+            ITestOutputHelper testOutputHelper)
+            : base(nameof(SourceFormatAddAsyncStepDefs), testOutputHelper)
+        {
+        }
+
+        [Given(@"there is a SourceFormat without RootDimensionStructure")]
+        public void ThereIsASourceFormatWithoutRootDimensionStructure()
+        {
+            _sourceFormat = new SourceFormat
+            {
+                Name = "source format without root dimension structure",
+                Desc = "description",
+                IsActive = 0,
+            };
+        }
+
+        [Given(@"there is a DimensionStructure")]
+        public void ThereIsADimensionStructure()
+        {
+            _dimensionStructure = new DimensionStructure
+            {
+                Name = "dimension structure",
+                Desc = "desc",
+                IsActive = 1,
+            };
+        }
+
+        [Given(@"DimensionStructure is RootDimensionStructure of SourceFormat")]
+        public void DimensionStructureIsRootDimensionStructureOfSourceFormat()
+        {
+            _sourceFormat.RootDimensionStructure = _dimensionStructure;
+        }
+
+        [When(@"SourceFormat is saved")]
+        public void SourceFormatIsSaved(){}
+        
+        [When(@"it returns with the newly added SourceFormat")]
+        public async Task ItReturnsWithTheNewlyAddedSourceFormat()
+        {
+            _sourceFormatResult = await _masterDataBusinessLogic.AddSourceFormatAsync(_sourceFormat)
+               .ConfigureAwait(false);
+        }
+
+        [Then(@"the returned SourceFormat is not null")]
+        public async Task TheReturnedSourceFormatIsNotNull()
+        {
+            _sourceFormatResult.Should().NotBeNull();
+        }
+
+        [Then(@"the returned SourceFormat's id should not be zero")]
+        public async Task TheReturnedSourceFormatsIdShouldNotBeZero()
+        {
+            _sourceFormatResult.Id.Should().NotBe(0);
+        }
+
+        [Then(@"the returned SourceFormat's name equals to the original's name")]
+        public async Task TheReturnedSourceFormatsNameEqualstoTheOriginalsName()
+        {
+            _sourceFormatResult.Name.Should().Be(_sourceFormat.Name);
+        }
+
+        [Then(@"the returned SourceFormat's description equals to the original's description")]
+        public async Task TheReturnedSourceFormatsDescriptionEqualsToTheOriginalsDescription()
+        {
+            _sourceFormatResult.Desc.Should().Be(_sourceFormat.Desc);
+        }
+
+        [Then(@"the returned SourceFormat's is active value equals to the original's is active value")]
+        public async Task TheReturnedSourceFormatsIsActiveValueEqualsToTheOriginalsIsActiveValue()
+        {
+            _sourceFormatResult.IsActive.Should().Be(_sourceFormat.IsActive);
+        }
+    }
+}
