99a9407a962f00f75a5a265d078e9fa5fcdf2dbd
SayusiAndo <sayusi.ando@gmail.com>
2020-12-30 18:56:04 +0100

    Feature: SourceFormatDimensionStructureNode REST Api - Update Method Input Validation
    Signed-off-by: SayusiAndo <sayusi.ando@gmail.com>

---
+++ src/MasterData/MasterData.Web.Api.Features/StepDefinitions/SourceFormatDimensionStructureNodeIsSavedOperationResultIsAnError.cs
@@ -0,0 +1,22 @@
+namespace DigitalLibrary.MasterData.Web.Api.Features.StepDefinitions
+{
+    using DigitalLibrary.Utils.MasterDataTestHelper.Entities;
+
+    using FluentAssertions;
+
+    using TechTalk.SpecFlow;
+    using TechTalk.SpecFlow.Assist;
+
+    public partial class StepDefinitions
+    {
+        [Then(@"SourceFormatDimensionStructureNode is saved operation result is an error")]
+        public void ThenSourceFormatDimensionStructureNodeIsSavedOperationResultIsAnError(Table table)
+        {
+            KeyResultKeyEntity instance = table.CreateInstance<KeyResultKeyEntity>();
+
+            int result = (int) _scenarioContext[instance.Key];
+
+            result.Should().Be(400);
+        }
+    }
+}
