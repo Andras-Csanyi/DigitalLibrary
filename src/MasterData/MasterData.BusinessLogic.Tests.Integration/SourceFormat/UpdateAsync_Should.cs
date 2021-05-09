b1917135eff8a184b1e39d8882e93a86099c0875
András Csányi <andras.csanyi@pm.me>
2021-05-09 19:07:57 +0200

    refactor: replaces specflow tests covering SourceFormat.UpdateAsync to integration tests (#231)
    * add missing excludecoverage attributes
    add integration tests covering SourceFormat UpdateAsync
    
    Signed-off-by: Andras Csanyi <andras.csanyi@pm.me>
    
    * add tests and remove not needed specflow tests
    
    Signed-off-by: Andras Csanyi <andras.csanyi@pm.me>
    
    * Nits
    
    Signed-off-by: Andras Csanyi <andras.csanyi@pm.me>
    
    * codecov 10% treshold
    
    Signed-off-by: Andras Csanyi <andras.csanyi@pm.me>
    
    * add excludefromcoverage
    
    Signed-off-by: Andras Csanyi <andras.csanyi@pm.me>

---
+++ src/MasterData/MasterData.BusinessLogic.Tests.Integration/SourceFormat/UpdateAsync_Should.cs
@@ -0,0 +1,55 @@
+namespace DigitalLibrary.MasterData.BusinessLogic.Tests.Integration.SourceFormat
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Threading.Tasks;
+
+    using DigitalLibrary.MasterData.DomainModel;
+    using DigitalLibrary.MasterData.Tests.TestData;
+
+    using FluentAssertions;
+
+    using Xunit;
+    using Xunit.Abstractions;
+
+    [ExcludeFromCodeCoverage]
+    public class UpdateAsync_Should : TestBase
+    {
+        [Theory]
+        [ClassData(typeof(SourceFormat_UpdateAsync_TestData))]
+        public async Task Update_SpecifiedEntity(
+            string name,
+            string desc,
+            int isActive)
+        {
+            // Arrange
+            SourceFormat orig = _sourceFormatFaker.Generate();
+            SourceFormat origResult = await _masterDataBusinessLogic
+               .MasterDataSourceFormatBusinessLogic
+               .AddAsync(orig)
+               .ConfigureAwait(false);
+            SourceFormat update = new SourceFormat
+            {
+                Id = origResult.Id,
+                Name = name,
+                Desc = desc,
+                IsActive = isActive,
+            };
+
+            // Action
+            SourceFormat sourceFormat = await _masterDataBusinessLogic
+               .MasterDataSourceFormatBusinessLogic
+               .UpdateAsync(update)
+               .ConfigureAwait(false);
+
+            // Assert
+            sourceFormat.Id.Should().Be(origResult.Id);
+            sourceFormat.Name.Should().Be(update.Name);
+            sourceFormat.Desc.Should().Be(update.Desc);
+            sourceFormat.IsActive.Should().Be(update.IsActive);
+        }
+
+        public UpdateAsync_Should(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+        }
+    }
+}
---
+++ src/MasterData/MasterData.Tests.TestData/SourceFormat_UpdateAsync_TestData.cs
@@ -0,0 +1,22 @@
+namespace DigitalLibrary.MasterData.Tests.TestData
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+
+    [ExcludeFromCodeCoverage]
+    public class SourceFormat_UpdateAsync_TestData : IEnumerable<object[]>
+    {
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { "asdasd", "asdasd", 1 };
+            yield return new object[] { "asdasd", "asdasd asdasd", 0 };
+            yield return new object[] { "asdasd asda", "asdasd asdasd", 1 };
+        }
+    }
+}
