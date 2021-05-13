baa110e0572c4b51e40987a2cf934a40426d5a36
Andras Csanyi <andras.csanyi@pm.me>
2021-05-13 20:17:20 +0200

    add further test covering delete SourceFormat
    Signed-off-by: Andras Csanyi <andras.csanyi@pm.me>

--- src/MasterData/MasterData.BusinessLogic.Tests.Integration/SourceFormat/DeleteAsync_Should.cs
+++ src/MasterData/MasterData.BusinessLogic.Tests.Integration/SourceFormat/DeleteAsync_Should.cs
@@ -89,13 +89,86 @@ namespace DigitalLibrary.MasterData.BusinessLogic.Tests.Integration.SourceFormat
         }
 
         [Fact]
-        public async Task CeaseConnection_BetweenSFAndDS_ButDSHasOtherConnections()
+        public async Task Deletes_TheFullDimensionStructureNodeTree()
         {
-        }
+            // Arrange
+            SourceFormat sourceFormatOrig = await CreateSavedSourceFormatEntity().ConfigureAwait(false);
+            DimensionStructureNode root = await CreateSavedDimensionStructureNodeEntity().ConfigureAwait(false);
+            await _masterDataBusinessLogic.MasterDataSourceFormatBusinessLogic
+               .AddRootDimensionStructureNodeAsync(sourceFormatOrig.Id, root.Id).ConfigureAwait(false);
 
-        [Fact]
-        public async Task Remove_AllDimensionStructureNodesBelongToSourceFormat()
-        {
+            DimensionStructureNode oneOnFirstLevel = await CreateSavedDimensionStructureNodeEntity()
+               .ConfigureAwait(false);
+            await _masterDataBusinessLogic.MasterDataSourceFormatBusinessLogic
+               .AppendDimensionStructureNodeToTreeAsync(oneOnFirstLevel.Id, root.Id, sourceFormatOrig.Id)
+               .ConfigureAwait(false);
+
+            DimensionStructureNode secondOnFirstLevel = await CreateSavedDimensionStructureNodeEntity()
+               .ConfigureAwait(false);
+            await _masterDataBusinessLogic.MasterDataSourceFormatBusinessLogic
+               .AppendDimensionStructureNodeToTreeAsync(secondOnFirstLevel.Id, root.Id, root.Id)
+               .ConfigureAwait(false);
+
+            DimensionStructureNode thirdOnFirstLevel = await CreateSavedDimensionStructureNodeEntity()
+               .ConfigureAwait(false);
+            await _masterDataBusinessLogic.MasterDataSourceFormatBusinessLogic
+               .AppendDimensionStructureNodeToTreeAsync(thirdOnFirstLevel.Id, root.Id, sourceFormatOrig.Id)
+               .ConfigureAwait(false);
+
+            DimensionStructureNode firstToTheFirst = await CreateSavedDimensionStructureNodeEntity()
+               .ConfigureAwait(false);
+            await _masterDataBusinessLogic.MasterDataSourceFormatBusinessLogic
+               .AppendDimensionStructureNodeToTreeAsync(firstToTheFirst.Id, oneOnFirstLevel.Id, sourceFormatOrig.Id)
+               .ConfigureAwait(false);
+
+            DimensionStructureNode secondToTheFirst = await CreateSavedDimensionStructureNodeEntity()
+               .ConfigureAwait(false);
+            await _masterDataBusinessLogic.MasterDataSourceFormatBusinessLogic
+               .AppendDimensionStructureNodeToTreeAsync(secondToTheFirst.Id, oneOnFirstLevel.Id, sourceFormatOrig.Id)
+               .ConfigureAwait(false);
+
+            DimensionStructureNode firstToTheSecond = await CreateSavedDimensionStructureNodeEntity()
+               .ConfigureAwait(false);
+            await _masterDataBusinessLogic.MasterDataSourceFormatBusinessLogic
+               .AppendDimensionStructureNodeToTreeAsync(firstToTheSecond.Id, secondOnFirstLevel.Id, sourceFormatOrig.Id)
+               .ConfigureAwait(false);
+
+            DimensionStructureNode secondToTheSecond = await CreateSavedDimensionStructureNodeEntity()
+               .ConfigureAwait(false);
+            await _masterDataBusinessLogic.MasterDataSourceFormatBusinessLogic
+               .AppendDimensionStructureNodeToTreeAsync(secondToTheSecond.Id, secondOnFirstLevel.Id,
+                    sourceFormatOrig.Id)
+               .ConfigureAwait(false);
+
+            DimensionStructureNode firstToTheThird = await CreateSavedDimensionStructureNodeEntity()
+               .ConfigureAwait(false);
+            await _masterDataBusinessLogic.MasterDataSourceFormatBusinessLogic
+               .AppendDimensionStructureNodeToTreeAsync(firstToTheThird.Id, thirdOnFirstLevel.Id, sourceFormatOrig.Id)
+               .ConfigureAwait(false);
+
+            DimensionStructureNode secondToTheThird = await CreateSavedDimensionStructureNodeEntity()
+               .ConfigureAwait(false);
+            await _masterDataBusinessLogic.MasterDataSourceFormatBusinessLogic
+               .AppendDimensionStructureNodeToTreeAsync(secondToTheThird.Id, thirdOnFirstLevel.Id, sourceFormatOrig.Id)
+               .ConfigureAwait(false);
+
+            // Action
+            await _masterDataBusinessLogic.MasterDataSourceFormatBusinessLogic
+               .DeleteAsync(sourceFormatOrig)
+               .ConfigureAwait(false);
+
+            // Assert
+            List<SourceFormat> sourceFormats = await _masterDataBusinessLogic
+               .MasterDataSourceFormatBusinessLogic
+               .GetAllAsync()
+               .ConfigureAwait(false);
+            sourceFormats.Count.Should().Be(0);
+
+            List<DimensionStructureNode> dimensionStructureNodes = await _masterDataBusinessLogic
+               .MasterDataSourceFormatBusinessLogic
+               .GetAllDimensionStructureNodesAsync()
+               .ConfigureAwait(false);
+            dimensionStructureNodes.Count.Should().Be(0);
         }
 
         public DeleteAsync_Should(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
