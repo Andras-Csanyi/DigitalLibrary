3961bb043515c699928dc89e41a77425ee492038
Andras Csanyi <andras.csanyi@pm.me>
2021-05-14 20:11:40 +0200

    add test covering adding child to the tree
    Signed-off-by: Andras Csanyi <andras.csanyi@pm.me>

--- src/MasterData/MasterData.BusinessLogic.Tests.Integration/SourceFormat/AddDimensionStructureNodeAsync_Should.cs
+++ src/MasterData/MasterData.BusinessLogic.Tests.Integration/SourceFormat/AddDimensionStructureNodeAsync_Should.cs
@@ -1,7 +1,12 @@
 namespace DigitalLibrary.MasterData.BusinessLogic.Tests.Integration.SourceFormat
 {
     using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
     using System.Threading.Tasks;
+
+    using DigitalLibrary.MasterData.DomainModel;
+
+    using FluentAssertions;
 
     using Xunit;
     using Xunit.Abstractions;
@@ -12,6 +17,25 @@ namespace DigitalLibrary.MasterData.BusinessLogic.Tests.Integration.SourceFormat
         [Fact]
         public async Task Add_ChildNodeToRootDsn()
         {
+            // Arrange
+            SourceFormat sf = await CreateSavedSourceFormatEntity().ConfigureAwait(false);
+            DimensionStructureNode root = await CreateSavedDimensionStructureNodeEntity().ConfigureAwait(false);
+            await _masterDataBusinessLogic.MasterDataSourceFormatBusinessLogic
+               .AddRootDimensionStructureNodeAsync(sf.Id, root.Id).ConfigureAwait(false);
+
+            DimensionStructureNode child = await CreateSavedDimensionStructureNodeEntity().ConfigureAwait(false);
+
+            // Action
+            await _masterDataBusinessLogic.MasterDataSourceFormatBusinessLogic
+               .AppendDimensionStructureNodeToTreeAsync(child.Id, root.Id, sf.Id)
+               .ConfigureAwait(false);
+
+            // Assert
+            SourceFormat result = await _masterDataBusinessLogic.MasterDataSourceFormatBusinessLogic
+               .GetSourceFormatByIdWithDimensionStructureNodeTreeAsync(sf)
+               .ConfigureAwait(false);
+            result.SourceFormatDimensionStructureNode.DimensionStructureNode.ChildNodes.ElementAt(0).Id
+               .Should().Be(child.Id);
         }
 
         public AddDimensionStructureNodeAsync_Should(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
