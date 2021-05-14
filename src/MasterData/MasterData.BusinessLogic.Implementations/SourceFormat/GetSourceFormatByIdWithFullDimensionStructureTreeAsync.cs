507dc2ee7ef4e02b788e39dd5f31fa0b5481cc38
Andras Csanyi <andras.csanyi@pm.me>
2021-05-14 19:41:40 +0200

    remove dead code
    Signed-off-by: Andras Csanyi <andras.csanyi@pm.me>

--- src/MasterData/MasterData.BusinessLogic.Implementations/SourceFormat/GetSourceFormatByIdWithFullDimensionStructureTreeAsync.cs
+++ src/MasterData/MasterData.BusinessLogic.Implementations/SourceFormat/GetSourceFormatByIdWithFullDimensionStructureTreeAsync.cs
@@ -6,7 +6,6 @@
 namespace DigitalLibrary.MasterData.BusinessLogic.Implementations.SourceFormat
 {
     using System;
-    using System.Collections.Generic;
     using System.Threading.Tasks;
 
     using DigitalLibrary.MasterData.BusinessLogic.Exceptions;
@@ -47,61 +46,6 @@ namespace DigitalLibrary.MasterData.BusinessLogic.Implementations.SourceFormat
                              $"For further details see inner exception.";
                 throw new MasterDataBusinessLogicDatabaseOperationException(msg, e);
             }
-        }
-
-        /// <summary>
-        ///     Returns tree of DimensionStructure below the given DimensionStructure defined by Id.
-        /// </summary>
-        /// <param name="dimensionStructureId">The top level DimensionStructure of tree</param>
-        /// <param name="ctx">MasterDataContext instance</param>
-        /// <returns>Tree of DimensionStructure</returns>
-        /// <exception cref="MasterDataBusinessLogicGetSourceFormatByIdWithDimensionStructureTreeAsyncOperationException"></exception>
-        private async Task<List<DimensionStructure>> GetDimensionStructureTreeAsync(
-            long? dimensionStructureId,
-            MasterDataContext ctx)
-        {
-            // try
-            // {
-            //     Check.IsNotNull(dimensionStructureId);
-            //     Check.AreNotEqual(dimensionStructureId, 0);
-            //
-            //     List<DimensionStructureDimensionStructure> result = ctx.DimensionStructureDimensionStructures
-            //        .AsNoTracking()
-            //        .Where(id => id.DimensionStructureId == dimensionStructureId)
-            //        .Where(child => child.ChildDimensionStructureId != 0)
-            //        .ToList();
-            //
-            //     if (result.Any())
-            //     {
-            //         List<DimensionStructure> dimensionStructures = new List<DimensionStructure>();
-            //         foreach (DimensionStructureDimensionStructure dimensionStructureDimensionStructure in result)
-            //         {
-            //             DimensionStructure dimensionStructure = await ctx.DimensionStructures
-            //                .AsNoTracking()
-            //                .Include(i => i.Dimension)
-            //                .Where(p => p.Id == dimensionStructureDimensionStructure.ChildDimensionStructureId)
-            //                .AsNoTracking()
-            //                .FirstOrDefaultAsync()
-            //                .ConfigureAwait(false);
-            //
-            //             // dimensionStructure.ChildDimensionStructures =
-            //             //     await GetDimensionStructureTreeAsync(dimensionStructure.Id, ctx)
-            //             //        .ConfigureAwait(false);
-            //
-            //             dimensionStructures.Add(dimensionStructure);
-            //         }
-            //
-            //         return dimensionStructures;
-            //     }
-            //
-            //     return new List<DimensionStructure>();
-            // }
-            // catch (Exception e)
-            // {
-            //     throw new MasterDataBusinessLogicGetSourceFormatByIdWithDimensionStructureTreeAsyncOperationException(
-            //         e.Message, e);
-            // }
-            throw new NotImplementedException();
         }
     }
 }
