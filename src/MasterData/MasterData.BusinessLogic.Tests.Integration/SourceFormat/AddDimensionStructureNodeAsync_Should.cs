16bddbef1baa8bf6737762660fc971d18a847c29
Andras Csanyi <andras.csanyi@pm.me>
2021-05-14 20:00:26 +0200

    move files around
    Signed-off-by: Andras Csanyi <andras.csanyi@pm.me>

---
+++ src/MasterData/MasterData.BusinessLogic.Tests.Integration/SourceFormat/AddDimensionStructureNodeAsync_Should.cs
@@ -0,0 +1,19 @@
+namespace DigitalLibrary.MasterData.BusinessLogic.Tests.Integration.SourceFormat
+{
+    using System.Threading.Tasks;
+
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class AddDimensionStructureNodeAsync_Should : TestBase
+    {
+        [Fact]
+        public async Task Add_ChildNodeToRootDsn()
+        {
+        }
+
+        public AddDimensionStructureNodeAsync_Should(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+        }
+    }
+}
